b47c510e2b8d823818eef5065244fb2799ffa845
xiuji1994 <499984532@qq.com>
2018-06-04 16:52:36 +0800

    修改了后台ui

--- dist/js/jq-load.js
+++ dist/js/jq-load.js
@@ -2,9 +2,10 @@ $('nav ul li ul li ul li a').on('click', function(e) {
   e.preventDefault();  // 阻止链接跳转
    var url = this.href;  // 保存点击的地址
 
-  $('nav  a.current').removeClass('current');    
-  $(this).addClass('current');                       
+  $('nav  a.active').removeClass('active');    
+  $(this).addClass('active');                       
  
  $('#container').remove();                          
   $('#content').load(url + ' #container').fadeIn('slow'); // 加载新内�?url地址与该地址下的选择器之间要有空�?表示该url下的#container
  });
+
